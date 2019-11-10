1617160537bb7fefc4b02f162eac3fcc8fad332b
Erica Stroude <strernzer@sympatico.ca>
2019-11-10 17:02:21 -0500

    update db url and remove background color

--- app.js
+++ app.js
@@ -17,23 +17,24 @@ var commentRoutes = require("./routes/comments"),
   campgroundRoutes = require("./routes/campgrounds"),
   indexRoutes = require("./routes/index");
 
-// mongoose.connect("mongodb://localhost:27017/yelp_camp_version13", {
-//   useNewUrlParser: true
-// });
-mongoose
-  .connect(
-    "mongodb+srv://me:howcani55@cluster0-rkjxu.mongodb.net/test?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true
-    }
-  )
-  .then(() => {
-    console.log("Connected to DB!");
-  })
-  .catch(err => {
-    console.log("ERROR: ", err.message);
-  });
+mongoose.connect(process.env.DATABASEURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+// mongoose
+//   .connect(
+//     "mongodb+srv://me:howcani55@cluster0-rkjxu.mongodb.net/test?retryWrites=true&w=majority",
+//     {
+//       useNewUrlParser: true,
+//       useCreateIndex: true
+//     }
+//   )
+//   .then(() => {
+//     console.log("Connected to DB!");
+//   })
+//   .catch(err => {
+//     console.log("ERROR: ", err.message);
+//   });
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
